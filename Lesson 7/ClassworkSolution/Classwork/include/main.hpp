6f6d973dc3097263a31dd65476ec814822bc35c4
Apollo-15 <rayl1x.gta@gmail.com>
2026-05-13 23:28:39 +0200

    first part of second part

--- Lesson 7/ClassworkSolution/Classwork/include/main.hpp
+++ Lesson 7/ClassworkSolution/Classwork/include/main.hpp
@@ -1,9 +1,28 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-#include <iostream>
+#include <list>
 #include <mutex>
 
-void Print(int& value, std::mutex& at, int num);
+class CList
+{
+private:
+	std::mutex m_mt;
+	std::list<int> m_arr;
+
+public:
+	CList() = default;
+
+	void Add(int value);
+	void Add(const std::list<int>& list);
+
+	void GetFrontElem(int& value);
+
+	~CList();
+};
+
+void Print(int numberOfThread, CList& list);
+
+void FillRandomNumbers(std::list<int>& list, size_t count);
 
 #endif
--- Lesson 7/ClassworkSolution/Classwork/src/main.cpp
+++ Lesson 7/ClassworkSolution/Classwork/src/main.cpp
@@ -3,38 +3,17 @@
 
 #include <mutex>
 #include <thread>
+#include <chrono>
+#include <random>
 
 
-void Print(int& value, std::mutex& mt, int num)
-{
-    for (size_t i = 0; i < 1000; i++)
-    {
-	    if (mt.try_lock())
-	    {
-		    value = num;
-		    std::cout << value << "\n";
-		    mt.unlock();
-	    }
-	    else
-	    {
-		    std::cout << "Not locked\n";
-	    }
+/*
+ * 
+ */
 
-    }
-}
 
 int main()
 {
-    int value;
-    std::mutex mt;
+	CVector v;
 
-    std::thread th1(Print, std::ref(value), std::ref(mt), 11111);
-    std::thread th2(Print, std::ref(value), std::ref(mt), 22222);
-    std::thread th3(Print, std::ref(value), std::ref(mt), 33333);
-
-    th1.join();
-    th2.join();
-    th3.join();
-
-    return 0;
 }
